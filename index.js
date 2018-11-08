d5da00e975600d9edac12c883b693499f1059edf
Patrick Burris <nchupacabra@gmail.com>
2018-11-07 20:12:42 -0600

    setting up dev server

--- index.js
+++ index.js
@@ -7,23 +7,38 @@ const generatePDF = require('./src/generatePDF');
  * Program Entry
  */
 const entry = argv => {
+  let isDev = false;
   program
     .version('0.1.1', '-v, --version')
     .usage('<config> <output>')
     .option(
       '-c, --config <file>',
-      'Path to the config yaml file, defaults to ./puppet.yml'
+      'Path to the config yaml file, defaults to ./puppet.yml',
+      'puppet.yml'
     )
     .option(
       '-o, --out <file>',
-      'Path to the output pdf file, defaults to output.pdf'
-    )
-    .parse(argv);
+      'Path to the output pdf file, defaults to ./output.pdf',
+      'output.pdf'
+    );
 
-  if (!program.config) program.config = './puppet.yml';
-  if (!program.out) program.out = 'output.pdf';
-  const { config, out } = program;
-  generatePDF(config, out);
+  program
+    .command('dev')
+    .description(
+      'run a development server to render the markup before generating the pdf'
+    )
+    .action(() => {
+      isDev = true;
+      console.log('Starting dev server...');
+      console.log(program.config, program.out);
+    });
+
+  program.parse(argv);
+
+  if (!isDev) {
+    const { config, out } = program;
+    generatePDF(config, out);
+  }
 };
 
 entry(process.argv);
